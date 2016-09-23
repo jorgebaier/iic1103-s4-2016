61f5e48d37634e176b0fc7800da52353c828e2d5
Jorge Baier <jabaier@gmail.com>
2016-09-23 07:58:12 -0300

    generador de lipogramas

--- clase0922/ejemplo.py
+++ clase0922/ejemplo.py
@@ -1,44 +1,47 @@
 import bigramas_ord
 
-def leer_prohibidas():
-    # retorna un string que contiene a las letras que
-    # el usuario ingresa
-    print("qué letras quieres omitir?")
-    p = ''
-    c = ''
-    while  c != '0':
-        c = input()
-        if c != '0':
-            p = p + c
-    return p
+# bigramas_ord.cargar_archivo() : carga archivo de datos
+# bigramas_ord.palabra_comun_seguida(palabra):
+#   retorna una palabra que ocurre frecuentemente despues
+#   de palabra
 
+def leer_prohibidas():
+    # retorna un string que contiene las letras prohibidas
+    print('qué letras quieres omitir? ')
+    s = ''
+    c = ''
+    while c != '0':
+        s = s + c
+        c = input()
+    return s
 
 def legal(prohibidas,palabra):
-    # retorna True si palabra no contiene letas que
-    # están en prohibidas, y False en caso contrario
+    # retorna True si la palabra no contiene
+    # ningun caracter en prohibidas
+    # retorna False en caso contrario
     for c in prohibidas:
         if c in palabra:
             return False
-    return True   
+    return True
 
 
-# esta una funcion bigramas_ord.cargar_archivo()
-# esta es la otra funcion bigramas_ord.palabra_comun_seguida("cuando"))
 
 bigramas_ord.cargar_archivo()
-cuantas_palabras=int(input("cuantas palabras quieres: "))
+
+cuantas_palabras = int(input('cuantas palabras quieres? '))
+palabra_inicial = input('palabra inicial? ')
 prohibidas = leer_prohibidas()
-inicio = input("palabra inicial: ")
 
-print("Letras prohibidas: ",prohibidas)
+print("Letras prohibidas:",prohibidas)
 
+contador = 0
 
-cont = 0
-
-palabra = inicio
-while cont < cuantas_palabras:
-    print(palabra,end=' ')
-    palabra = bigramas_ord.palabra_comun_seguida(palabra)
+palabra = palabra_inicial
+while contador < cuantas_palabras:
+    print(palabra, end=' ')
+    palabra_original = palabra
+    palabra = bigramas_ord.palabra_comun_seguida(palabra_original)
     while not legal(prohibidas,palabra):
-        palabra = bigramas_ord.palabra_comun_seguida(palabra)
-    cont = cont + 1
+        palabra = bigramas_ord.palabra_comun_seguida(palabra_original)    
+    contador += 1 # contador = contador + 1
+
